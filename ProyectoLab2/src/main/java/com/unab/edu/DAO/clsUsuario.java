760d6fe7c8de2488968726cc3652c0234c5c86b7
MilitoJR <62968166+MilitoJR@users.noreply.github.com>
2020-10-26 12:12:54 -0600

    Create clsUsuario.java

---
+++ ProyectoLab2/src/main/java/com/unab/edu/DAO/clsUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.DAO;
+
+import com.unab.edu.Conexion.ConexionBD;
+
+/**
+ *
+ * @author Milton
+ */
+public class clsUsuario {
+    ConexionBD conn = new ConexionDB();
+}
